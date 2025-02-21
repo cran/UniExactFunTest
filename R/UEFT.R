b189a047da604c003cef576ab210b21533c906ea
Yiyi Li <gtarex@nmsu.edu>
2025-02-21 07:00:02 +0000

    version 1.0.1

--- R/UEFT.R
+++ R/UEFT.R
@@ -1,7 +1,7 @@
 # Functions created for user
 # Exact functional functions include EFT, UEFT
 # Yiyi Li
-# 2023-09-10
+# 2023-10-11
 
 # Uniform exact function test
 #' @name UEFT
@@ -21,12 +21,25 @@
 #' The exact p-value of the test.
 #' @details
 #' The uniform idea was implementated using uniform marginal distribution of a square table as null hypothesis.
-#' The continuity correction algorithm
 #'
 #' @author Yiyi Li, Joe Song
 #' @useDynLib UniExactFunTest
 #' @import Rcpp
 #' @export
+#' @note
+#' The functions provide a direct entry into the C++ implementations of the exact functional test.
+#'
+#' @examples
+#'  # Initial a table
+#'  x = matrix(c(0,5,10,0,0,5), ncol=3)
+#'  # With continuity correction
+#'  UEFT(x)
+#'  # Without continuity correction
+#'  UEFT(x, FALSE)
+#' @keywords nonparametric
+#' @keywords exact test
+#' @keywords functional
+#' @keywords uniform
 UEFT = function(input, correct = TRUE, log.p = FALSE){
   DNAME <- deparse(substitute(input))
   if(correct){
@@ -35,20 +48,34 @@ UEFT = function(input, correct = TRUE, log.p = FALSE){
     method.text <- "Uniform Exact functional test"
   }
 
-  if(sum(input%%1!=0)>=1) {
+  theSum = sum(input)
+
+  if( is.na(theSum) | is.null(theSum) | sum(input%%1!=0)>=1) {
     # Check whether numbers in x are all integers
     stop("ERROR: Exact test requires integer contingency tables!", call. = TRUE)
   }
   # Check sample size
   fun.chisq = getFunChisqStat(input)
+  col.sums = colSums(input)
+  colChisq = ifelse(theSum > 0, sum(col.sums^2) / theSum * ncol(input) - theSum, 0)
+
+  maxFunChisq = theSum * ncol(input) - theSum - colChisq
+
+  if(maxFunChisq == 0){
+    estimate = 0
+  }else{
+    estimate = sqrt(abs(fun.chisq) / maxFunChisq)
+  }
+
+
   names(fun.chisq) <- "statistic"
 
-  if(sum(input) >= 150){
+  if(theSum >= 150){
     warning("UEFT requires sample size < 150. Switch to Funchisq")
     df = (nrow(input)-1) * (ncol(input)-1)
     p.value = stats::pchisq( fun.chisq, df = df, lower.tail=FALSE, log.p=log.p )
 
-    result = structure(list(statistic = fun.chisq, p.value = p.value, estimate = fun.chisq,
+    result = structure(list(statistic = fun.chisq, p.value = p.value, estimate = estimate,
                             data.name = DNAME, method = method.text),
                        class = "htest")
     return( result )
@@ -64,7 +91,7 @@ UEFT = function(input, correct = TRUE, log.p = FALSE){
   if(log.p) p.value <- log(p.value)
 
   names(fun.chisq) <- "statistic"
-  return(structure(list(statistic = fun.chisq, p.value = p.value, estimate = fun.chisq,
+  return(structure(list(statistic = fun.chisq, p.value = p.value, estimate = estimate,
                         data.name = DNAME, method = method.text),
                    class = "htest"))
 }
